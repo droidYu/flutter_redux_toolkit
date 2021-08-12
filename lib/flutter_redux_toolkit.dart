3ef29f282d145e9d1affdb4ca6a94c7ea16e6c71
trinhhuyit@gmail.com <trinhhuyit@gmail.com>
2021-08-12 08:36:50 +0700

    Added BaseRepository

--- lib/flutter_redux_toolkit.dart
+++ lib/flutter_redux_toolkit.dart
@@ -188,3 +188,12 @@ class BaseAsyncActionCreatorNoParam<AppState, Model> {
         }
       };
 }
+
+
+abstract class BaseRepository<T> {
+  Future<List<T>> find();
+  Future insert(T t);
+  Future<T> findOne(String id);
+  void remove(String id);
+  Future update(T t);
+}
